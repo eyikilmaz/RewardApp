abb899031dd0f6bc817260ec4ed5ed2d582d7ee2
eyikilmaz <yeyikilmaz@gmail.com>
2023-01-11 22:46:19 +0300

    features:  serilog implementation

--- src/Api/WebApi/RewardApp.Api.WebApi/Program.cs
+++ src/Api/WebApi/RewardApp.Api.WebApi/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using RewardApp.Api.Application.Extensions;
 using RewardApp.ExceptionHandlingExtension.Extensions;
 using RewardApp.Infrastructure.Persistence.Extensions;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,14 +20,22 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationRegistration();
 builder.Services.AddInfrastructureRegistration(builder.Configuration);
 
+
+builder.Host.UseSerilog((ctx, lc) =>
+{
+    lc.ReadFrom.Configuration(ctx.Configuration);
+});
+
+// test 
+//builder.Services.AddLogging();
+
 var app = builder.Build();
 
 // man
 app.ConfigureExceptionHandling(opt =>
 {
-    var logger = app.Services.GetService<ILogger>();
+    var logger = app.Services.GetService<Serilog.ILogger>();
     opt.UseExceptionDetails = true;
-    opt.UseLogger(logger);
 });
 
 // Configure the HTTP request pipeline.
@@ -35,7 +44,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 
 app.UseHttpsRedirection();
 
