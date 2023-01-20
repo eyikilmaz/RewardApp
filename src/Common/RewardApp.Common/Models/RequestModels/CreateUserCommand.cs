93ce67e98e96781ef38825128a39f62f363d2647
eyikilmaz <yeyikilmaz@gmail.com>
2023-01-20 20:31:08 +0300

    fixes:  create command handler missing fields added

--- src/Common/RewardApp.Common/Models/RequestModels/CreateUserCommand.cs
+++ src/Common/RewardApp.Common/Models/RequestModels/CreateUserCommand.cs
@@ -14,5 +14,7 @@ public class CreateUserCommand : IRequest<Guid>
     public string LastName { get; set; }
     public string EmailAddress { get; set; }
     public string UserName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
     public string Password { get; set; }
 }
